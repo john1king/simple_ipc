bf96f0a155122a11a246b3bbcff423343bbad1ee
zhangyijin <zhangyijin@bilibili.com>
2015-07-10 16:48:06 +0800

    socket文件已经存在时检查服务是否正在运行

--- server.rb
+++ server.rb
@@ -1,14 +1,37 @@
+# 最简单的，不支持持久连接，只能单向通信
 
 require "socket"
-# 最简单的，不支持持久连接
+
+SOCKET_FILE = "/tmp/oh_my_sock"
+
+def server_exist?
+  UNIXSocket.open(SOCKET_FILE) {|c|
+    c.puts "check"
+    c.close
+  }
+  true
+rescue Errno::ECONNREFUSED
+  false
+end
 
 
-UNIXServer.open("/tmp/oh_my_sock") {|serv|
-  loop do
-    s = serv.accept
-    puts "accept #{s}"
-    string = s.read
-    puts string
-    s.close
+begin
+  UNIXServer.open(SOCKET_FILE) {|serv|
+    loop do
+      s = serv.accept
+      puts "accept #{s}"
+      string = s.read
+      puts string
+      s.close
+    end
+  }
+rescue Errno::EADDRINUSE
+  unless server_exist?
+    puts "Remove #{SOCKET_FILE}"
+    File.delete(SOCKET_FILE)
+    retry
   end
-}
+  puts "A server is already running."
+end
+
+
